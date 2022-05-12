2c72322a07364fff9367d8682ff832a46839a2e1
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-05-12 13:54:51 +0545

    mongo

---
+++ 5-CRUD-w-mongo/app.js
@@ -0,0 +1,7 @@
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/playground') 
+    .then(()=>console.log("Connected to MongoDB..."))
+    .catch(err=>console.log(`Couldnot connect to MongoDb....`,err))
+
